fd0508a164059dc12f25041be01af549a004d49c
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-09-07 17:45:10 -0700

    added in some CSS and added the const into the JavaScript.

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey = 'da3fd98c9dbfd54a3bbcabffb7ac3ad5';
+const searchButton = document.getElementById('search-button');
+const cityInput = document.getElementById('city-input');
+const weatherContainer = document.getElementById('weather-container');
+const savedCitiesList = document.getElementById('saved-cities-list');
